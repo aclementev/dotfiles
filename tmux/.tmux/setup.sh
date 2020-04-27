151b032333231f1bb72645cc66dcf6441d61e04c
Alvaro Clemente <clemente@bigml.com>
2020-04-27 12:26:46 +0200

    Include tmux config

---
+++ tmux/.tmux/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check that git is installed
+if which git &> /dev/null
+then
+    # Get this script's directory
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    git clone https://github.com/tmux-plugins/tpm $DIR/plugins/tpm
+else
+    echo "git must be installed to run this script"
+fi
