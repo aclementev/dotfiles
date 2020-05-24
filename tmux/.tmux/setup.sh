e01028ef2322d57e254319948c7e89f7764c123d
Alvaro Clemente <clemente.varo@gmail.com>
2020-05-24 19:13:36 +0200

    Add stow install step to tmux setup

--- tmux/.tmux/setup.sh
+++ tmux/.tmux/setup.sh
@@ -6,6 +6,7 @@ then
     # Get this script's directory
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
     git clone https://github.com/tmux-plugins/tpm $DIR/plugins/tpm
+    stow -d ~/dotfiles tmux
 else
     echo "git must be installed to run this script"
 fi
