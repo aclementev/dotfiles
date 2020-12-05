1ec8825cb974c45cb585f0a56d3e131f69fea924
Alvaro Clemente <clemente@bigml.com>
2020-12-05 10:57:08 +0100

    nvim: add custom Lua utils

--- nvim/.config/nvim/lua/alvaro/init.lua
+++ nvim/.config/nvim/lua/alvaro/init.lua
@@ -1,1 +1,9 @@
-print('hello from lua!')
+-- An alias easier to type
+function inspect(...)
+    print(vim.inspect(...))
+end
+
+-- Show the current clients attached the the
+function buf_show_clients()
+    print(vim.inspect(vim.lsp.buf_get_clients()))
+end
