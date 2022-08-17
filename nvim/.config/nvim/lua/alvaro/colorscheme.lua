db9dcf9dd794c3048bd13fb4454a2153b7fbe064
Alvaro Clemente <clemente@bigml.com>
2022-08-17 19:41:59 +0200

    refactor(nvim): move colors to lua

---
+++ nvim/.config/nvim/lua/alvaro/colorscheme.lua
@@ -0,0 +1,8 @@
+-- Colorscheme setup
+vim.o.termguicolors = true
+
+-- TODO(alvaro): Read the environment to pick light or dark
+vim.o.background = 'dark'
+vim.g.ayucolor = 'mirage'
+
+vim.cmd [[ colorscheme ayu ]]
