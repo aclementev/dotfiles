4dacafe230560c7ac69b8b66ae91d2be50bcb9b3
Alvaro Clemente <clemente@bigml.com>
2022-08-19 13:39:19 +0200

    feat(nvim): add `RefreshColorscheme` command

--- nvim/.config/nvim/lua/alvaro/colorscheme.lua
+++ nvim/.config/nvim/lua/alvaro/colorscheme.lua
@@ -1,12 +1,17 @@
 -- Colorscheme setup
 -- We use a simple file based system to automatically detect the preference
 -- of light vs dark mode for the editor colorscheme
+-- TODO(alvaro): Automatically detect changes of the screen mode
+-- Can we set up a watcher on the `SCREEN_MODE_FILE` to listen to file
+-- changes?
+-- Another option is to Refresh the colorscheme on some kind of WinEnter,
+-- TabEnter, FocusGained events so that it's lazy
 vim.o.termguicolors = true
 
 local DEFAULT_MODE = 'dark'
 local SCREEN_MODE_FILE = vim.fn.expand("~/.alvaro_screen_mode")
 
-local function read_mode()
+local function load_mode()
     if vim.fn.filereadable(SCREEN_MODE_FILE) == 0 then
         -- No file specified, so just return the default
         return DEFAULT_MODE
@@ -30,18 +35,24 @@ local function read_mode()
     end
 end
 
-local mode = read_mode()
+function RefreshColorscheme()
+    local mode = load_mode()
+    if mode == 'dark' then
+        -- Apply the ayu-mirage colorscheme
+        vim.o.background = 'dark'
+        vim.g.ayucolor = 'mirage'
+        vim.cmd [[ colorscheme ayu ]]
+    elseif mode == 'light' then
+        -- Apply the solarized light colorscheme
+        vim.o.background = 'light'
+        vim.cmd [[ colorscheme solarized ]]
+    else
+        print("Unexpected color mode value! '" .. mode .. "'")
+    end
+end
+
+-- Register a command for refreshing the colorscheme
+vim.api.nvim_create_user_command('RefreshColorscheme', RefreshColorscheme, { nargs = 0 })
 
 -- Actually apply the colorscheme
-if mode == 'dark' then
-    -- Apply the ayu-mirage colorscheme
-    vim.o.background = 'dark'
-    vim.g.ayucolor = 'mirage'
-    vim.cmd [[ colorscheme ayu ]]
-elseif mode == 'light' then
-    -- Apply the solarized light colorscheme
-    vim.o.background = 'light'
-    vim.cmd [[ colorscheme solarized ]]
-else
-    print("Unexpected color mode value! '" .. mode .. "'")
-end
+RefreshColorscheme()
