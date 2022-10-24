22e02b6fd68e7bb4f55d5110eaea88b879701af0
Alvaro Clemente <clemente@bigml.com>
2022-10-24 16:28:00 +0200

    fix(nvim): breaking change for capabilities

--- nvim/.config/nvim/lua/alvaro/lsp/init.lua
+++ nvim/.config/nvim/lua/alvaro/lsp/init.lua
@@ -104,8 +104,7 @@ local on_attach_general = function(client)
 end
 
 -- Update the capabilities as suggested by `cmp-nvim-lsp`
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- sumneko_lua
 lspconfig.sumneko_lua.setup({
