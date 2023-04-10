9626fa0b17879af542ff7bbb06f6ccb0d361f49d
Alvaro Clemente <clemente@bigml.com>
2023-04-10 10:50:29 +0200

    feat(env): add aliases

--- environment/.system-env.sh
+++ environment/.system-env.sh
@@ -1,15 +1,34 @@
-# This file should contain configuration variables that are meant to be 
-# shared across systems. 
-# To override some configuration, use `.system-env-specific.sh`
-echo "Hello from .system-env.sh"
+# This file should contain configuration variables that are meant to be
+# shared across systems.
+# To override some configuration, create a `$HOME/.system-env-specific.sh`
+# config file
 
 # ----------- System Settings ------------
 export EDITOR="nvim"
 
-# ------------- System Tools ------------- 
+# ------------- System Tools -------------
 # FZF
-[ -e "$HOME/.fzf.bash" ] && . "$HOME/.fzf.bash"
+if [[ "$SHELL" = "/bin/zsh" ]]; then
+    [ -e "$HOME/.fzf.zsh" ] && . "$HOME/.fzf.zsh"
+else
+    [ -e "$HOME/.fzf.bash" ] && . "$HOME/.fzf.bash"
+fi
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
+export FZF_DEFAULT_COMMAND='rg --files --hidden'
+
+# direnv
+if [[ -x "$(command -v direnv)" ]]; then
+    if [[ "$SHELL" = "/bin/zsh" ]]; then
+        eval "$(direnv hook zsh)"
+    else
+        eval "$(direnv hook bash)"
+    fi
+fi
+
+# Custom tools
+if [ -d "$HOME/code/tools/bin/" ]; then
+    export PATH="$HOME/code/tools/bin:$PATH"
+fi
 
 # Z
 [ -e "$HOME/.local/bin/z.sh" ] && . "$HOME/.local/bin/z.sh"
@@ -19,12 +38,14 @@ export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 [ -e "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
 
 # ---------------- NodeJS ----------------
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# NOTE(alvaro): This is commented out since it's very slow, use lazy loading
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # ---------------- Python ----------------
 # TODO: Migrate over to pyenv everywhere (?)
+export WORKON_HOME=~/.virtualenv
 
 # ---------------- Haskell ---------------
 [ -e "$HOME/.ghcup/env" ] && . "$HOME/.ghcup/env"
@@ -34,6 +55,5 @@ export NVM_DIR="$HOME/.nvm"
 
 # Load any system specific overrides
 if [ -e "$HOME/.system-env-specific.sh" ]; then
-    echo 'Loading system specific overrides'
     . "$HOME/.system-env-specific.sh"
 fi
