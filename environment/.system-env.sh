97a7cae510d40f155d2cee81c89272bc6488706b
Alvaro Clemente <clemente.varo@gmail.com>
2024-05-05 14:49:49 +0200

    fix(env): simplify base env config

--- environment/.system-env.sh
+++ environment/.system-env.sh
@@ -29,8 +29,6 @@ if [[ -x "$(command -v direnv)" ]]; then
     fi
 fi
 
-
-
 # Custom tools
 if [ ! -d "$HOME/.local/bin/" ]; then
     mkdir -p "$HOME/.local/bin"
@@ -40,30 +38,6 @@ export PATH="$HOME/.local/bin:$PATH"
 if [ -d "$HOME/code/tools/bin/" ]; then
     export PATH="$HOME/code/tools/bin:$PATH"
 fi
-
-# Z
-[ -e "$HOME/.local/bin/z.sh" ] && . "$HOME/.local/bin/z.sh"
-
-# ----------------- Rust -----------------
-# Setting up cargo
-[ -e "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
-
-# ---------------- NodeJS ----------------
-# NOTE(alvaro): This is commented out since it's very slow, use lazy loading
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# ---------------- Python ----------------
-# TODO: Migrate over to pyenv everywhere (?)
-export WORKON_HOME=~/.virtualenv
-export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-
-# ---------------- Haskell ---------------
-[ -e "$HOME/.ghcup/env" ] && . "$HOME/.ghcup/env"
-
-# ---------------- BigML -----------------
-[ -f "$HOME/.bigml_credentials" ] && . "$HOME/.bigml_credentials"
 
 # Load any system specific overrides
 if [ -e "$HOME/.system-env-specific.sh" ]; then
