ece3150c2164f3fd201e9c05b84853a1c1478b54
Alvaro Clemente <clemente.varo@gmail.com>
2026-04-11 23:12:10 +0200

    feat(nvim): status / buffer line

--- nvim/.config/nvim/lua/plugins/eye-candy.lua
+++ nvim/.config/nvim/lua/plugins/eye-candy.lua
@@ -19,4 +19,30 @@ return {
       legacy_computing_symbols_support = true,
     },
   },
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+  },
+  -- bufferline
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      options = {
+        diagnostics = "nvim_lsp",
+        diagnostics_update_in_insert = false,
+        show_close_icon = false,
+        show_buffer_close_icons = false,
+        separator_style = "padded_slant",
+        hover = {
+          enabled = true,
+          delay = 200,
+          reveal = { "close" },
+        },
+      },
+    },
+  },
 }
