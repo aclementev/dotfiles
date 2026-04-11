6800c9e3e2c8a3754fca7dd67f7adae4ea3f4001
Alvaro Clemente <clemente.varo@gmail.com>
2026-04-11 19:08:39 +0200

    feat(nvim): file navigation

---
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -0,0 +1,178 @@
+return {
+    { 
+        "stevearc/oil.nvim",
+        -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+        lazy = false,
+        -- Optional dependencies
+        dependencies = { "nvim-mini/mini.icons"  },
+        -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {
+            keymaps = {
+                ["g?"] = { "actions.show_help", mode = "n" },
+                ["<CR>"] = "actions.select",
+                ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+                -- This is a modification of the default <C-h> one
+                ["<C-x>"] = { "actions.select", opts = { horizontal = true } },
+                ["<C-t>"] = { "actions.select", opts = { tab = true } },
+                ["<C-p>"] = "actions.preview",
+                ["<C-c>"] = { "actions.close", mode = "n" },
+                -- This is a modification of the default <C-l> one
+                ["<C-r>"] = "actions.refresh",
+                ["-"] = { "actions.parent", mode = "n" },
+                ["_"] = { "actions.open_cwd", mode = "n" },
+                ["`"] = { "actions.cd", mode = "n" },
+                ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+                ["gs"] = { "actions.change_sort", mode = "n" },
+                ["gx"] = "actions.open_external",
+                ["g."] = { "actions.toggle_hidden", mode = "n" },
+                ["g\\"] = { "actions.toggle_trash", mode = "n" },
+            },
+            use_default_keymaps = false,
+        },
+        keys = {
+            { "g-", function() require("oil").toggle() end, desc = "Toggle Oil.nvim" },
+            { "g.", function() 
+                require("oil").toggle_float(vim.fs.root(0, { ".git", ".hg" }) or vim.uv.cwd()) 
+            end, desc = "Toggle Oil.nvim on repo root" },
+        }
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        init = function()
+            vim.g.loaded_netrw = 1
+            vim.g.loaded_netrwPlugin = 1
+        end,
+        opts = {
+            sort_by = "case_sensitive",
+            view = {
+                side = "right",
+                adaptive_size = true,
+            },
+            renderer = {
+                group_empty = true,
+            },
+            filters = {
+                dotfiles = false,
+            },
+        },
+        keys = {
+            { "<Leader>b", ":NvimTreeFindFileToggle<CR>", desc = "Open NvimTree on the current file" },
+        },
+    },
+    {
+        {
+            "ThePrimeagen/harpoon",
+            dependencies = { "nvim-lua/plenary.nvim" },
+            branch = "harpoon2",
+            config = function()
+                local harpoon = require "harpoon"
+                harpoon:setup {
+                    settings = {
+                        save_on_toggle = true,
+                        sync_on_ui_close = true,
+                    },
+                    menu = {
+                        width = 120,
+                        height = 20,
+                    },
+                }
+            end,
+            keys = {
+                {
+                    "<LocalLeader>ha",
+                    function()
+                        require("harpoon"):list():add()
+                    end,
+                    desc = "Harpoon: Add file",
+                },
+                {
+                    "<LocalLeader>hh",
+                    function()
+                        local harpoon = require "harpoon"
+                        harpoon.ui:toggle_quick_menu(harpoon:list())
+                    end,
+                    desc = "Harpoon: Open Quick Menu",
+                },
+                {
+                    "<LocalLeader>hn",
+                    function()
+                        require("harpoon"):list():next()
+                    end,
+                    desc = "Harpoon: Navigate to next",
+                },
+                {
+                    "<LocalLeader>hp",
+                    function()
+                        require("harpoon"):list():prev()
+                    end,
+                    desc = "Harpoon: Navigate to previous",
+                },
+                {
+                    "<LocalLeader>1",
+                    function()
+                        require("harpoon"):list():select(1)
+                    end,
+                    desc = "Harpoon: Navigate to entry #1",
+                },
+                {
+                    "<LocalLeader>2",
+                    function()
+                        require("harpoon"):list():select(2)
+                    end,
+                    desc = "Harpoon: Navigate to entry #2",
+                },
+                {
+                    "<LocalLeader>3",
+                    function()
+                        require("harpoon"):list():select(3)
+                    end,
+                    desc = "Harpoon: Navigate to entry #3",
+                },
+                {
+                    "<LocalLeader>4",
+                    function()
+                        require("harpoon"):list():select(4)
+                    end,
+                    desc = "Harpoon: Navigate to entry #4",
+                },
+                {
+                    "<LocalLeader>5",
+                    function()
+                        require("harpoon"):list():select(5)
+                    end,
+                    desc = "Harpoon: Navigate to entry #5",
+                },
+                {
+                    "<LocalLeader>6",
+                    function()
+                        require("harpoon"):list():select(6)
+                    end,
+                    desc = "Harpoon: Navigate to entry #6",
+                },
+                {
+                    "<LocalLeader>7",
+                    function()
+                        require("harpoon"):list():select(7)
+                    end,
+                    desc = "Harpoon: Navigate to entry #7",
+                },
+                {
+                    "<LocalLeader>8",
+                    function()
+                        require("harpoon"):list():select(8)
+                    end,
+                    desc = "Harpoon: Navigate to entry #8",
+                },
+                {
+                    "<LocalLeader>9",
+                    function()
+                        require("harpoon"):list():select(9)
+                    end,
+                    desc = "Harpoon: Navigate to entry #9",
+                },
+            },
+        }
+    }
+}
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -23,6 +23,7 @@ return {
             {"<Leader>fw", function() Snacks.picker.lsp_workspace_symbols() end, desc = "Find LSP workspace symbols"},
             {"<Leader>fr", function() Snacks.picker.lsp_references() end, desc = "Find LSP references"},
             {"<Leader>fe", function() Snacks.picker.diagnostics_buffer() end, desc = "Find diagnostics (Buffer)"},
+            {"<Leader>fp", function() Snacks.picker.pickers() end, desc = "Find all pickers"},
             {"<Leader>fE", function() Snacks.picker.diagnostics() end, desc = "Find diagnostics"},
             {"<Leader>fe", function() Snacks.picker.icons() end, desc = "Find emoji"},
             {"<Leader>rr", function() Snacks.picker.grep() end, desc = "Live Grep"},
